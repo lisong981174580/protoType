ac1aa5b134050a9e71dbc57515084d3b156bdda7
李松 <981174580@qq.com>
2019-04-02 13:31:22 +0800

    提交zancun'

--- Array.js
+++ Array.js
@@ -1,7 +1,7 @@
 
 Array.prototype.includes=function(element){
    return this.indexOf(element)!==-1;
-}
+};
 
 Array.prototype.map=function(fn,context){
   let newArr=[];
@@ -9,5 +9,22 @@ Array.prototype.map=function(fn,context){
      newArr.push(fn.call(context,this[i],i,this))
   }
   return newArr
-}
+};
+
+Array.prototype.forEach=function(fn,context){
+  for(let i=0;i<=this.length-1;i++){
+    fn.call(context,this[i],i,this)
+  }
+};
+
+Array.prototype.filter=function(fn,context){
+  let newArr=[];
+  for(let i=0;i<=this.length-1;i++){
+    if(fn.call(context,this[i],i,this)){
+      newArr.push(this[i])
+    }
+  }
+  return newArr
+};
+
 
